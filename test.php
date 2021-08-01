6242dccdfec016268845acccb1cc54c8d7616a51
himanshu-itmca <himanshu.itmca@gmail.com>
2021-08-01 08:51:49 +0530

    add two files

---
+++ test.php
@@ -0,0 +1,29 @@
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+
+<center>
+	<hr>
+<div class="w3-text-blue">
+	<h4>fdgdf</h4>
+	<table class="table table-bordered" >
+		
+		<tr>
+			<td>1</td>
+			<td>10</td>
+			<td>10</td>
+		</tr>
+		<tr>
+			<td>2</td>
+			<td>20</td>
+			<td>20</td>
+		</tr>
+
+	</table>		
+		<h4>Get</h4>
+
+</div>
+</center>
+<hr>
